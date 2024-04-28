5250fd40f46c7ef7aff353bf819804acda972cad
xBookbig <koonbookza2015@gmail.com>
2024-04-29 00:33:21 +0700

    add : redux , type

--- src/component/ButtonAddList/ModalAdd.tsx
+++ src/component/ButtonAddList/ModalAdd.tsx
@@ -1,54 +1,65 @@
 import React, { useState } from 'react';
 import {  Input , Button, Modal } from 'antd';
+import { useDispatch } from 'react-redux';
+import { addTodo  } from '../store/action';
 
 const { TextArea } = Input;
 
 function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; }) {
-    const [loading, setLoading] = useState(false);
-    const [description, setDescription] = useState('');
-    const [title, setTitle] = useState('');
-
-    const addList = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        onClose();
-      }, 1000);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [description, setDescription] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const addList = () => {
+    setLoading(true);
+    const newTodo = {
+      key: Date.now(),
+      title,
+      description,
+      status: ['process'],
     };
-  
-    return (
-      <Modal
-        open={onOpen}
-        title="Add List New"
-        onOk={addList}
-        onCancel={onClose}
-        footer={[
-          <Button key="submit" type="primary" loading={loading} onClick={addList}>
-            Add
-          </Button>,
-          <Button key="back" onClick={onClose}>
-            Cancel
-          </Button>,
-        ]}
-      >
-        <>
-        <Input 
-            showCount 
-            maxLength={20} 
-            value={title} 
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title" 
-        />
-        <div className="my-6" />
-        <TextArea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Description"
-            autoSize={{ minRows: 3, maxRows: 5 }}
-        />
-        </>
-      </Modal>
-    );
+    dispatch(addTodo(newTodo));
+    setDescription('');
+    setTitle('');
+    setTimeout(() => {
+      setLoading(false);
+      onClose();
+    }, 100);
+  };
+
+  return (
+    <Modal
+      open={onOpen}
+      title="Add List New"
+      onOk={addList}
+      onCancel={onClose}
+      footer={[
+        <Button key="submit" type="primary" loading={loading} onClick={addList}>
+          Add
+        </Button>,
+        <Button key="back" onClick={onClose}>
+          Cancel
+        </Button>,
+      ]}
+    >
+      <>
+      <Input 
+          showCount 
+          maxLength={20} 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title" 
+      />
+      <div className="my-6" />
+      <TextArea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+          autoSize={{ minRows: 3, maxRows: 5 }}
+      />
+      </>
+    </Modal>
+  );
 }
 
 export default ModalAdd;
