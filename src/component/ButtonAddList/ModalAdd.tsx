36172dc61528a0a5c011600a63c00df3a7146464
xBookbig <koonbookza2015@gmail.com>
2024-04-29 00:57:50 +0700

    add : alert and check input

--- src/component/ButtonAddList/ModalAdd.tsx
+++ src/component/ButtonAddList/ModalAdd.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {  Input , Button, Modal } from 'antd';
+import {  Input , Button, Modal , Alert  } from 'antd';
 import { useDispatch } from 'react-redux';
 import { addTodo  } from '../store/action';
 
@@ -10,7 +10,16 @@ function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; 
   const [loading, setLoading] = useState<boolean>(false);
   const [description, setDescription] = useState<string>('');
   const [title, setTitle] = useState<string>('');
+  const [alert, setAlert] = useState<boolean>(false);
   const addList = () => {
+    if (title.length <= 0) {
+      setAlert(true);
+      return
+    }
+    if (description.length <= 0) {
+      setAlert(true);
+      return
+    }
     setLoading(true);
     const newTodo = {
       key: Date.now(),
@@ -20,6 +29,7 @@ function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; 
     };
     dispatch(addTodo(newTodo));
     setDescription('');
+    setAlert(false);
     setTitle('');
     setTimeout(() => {
       setLoading(false);
@@ -32,7 +42,7 @@ function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; 
       open={onOpen}
       title="Add List New"
       onOk={addList}
-      onCancel={onClose}
+      onCancel={() => { onClose(); setAlert(false); }}
       footer={[
         <Button key="submit" type="primary" loading={loading} onClick={addList}>
           Add
@@ -48,7 +58,7 @@ function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; 
           maxLength={80} 
           value={title} 
           onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title" 
+          placeholder="Title"
       />
       <div className="my-6" />
       <TextArea
@@ -57,6 +67,13 @@ function ModalAdd({ onOpen , onClose }: { onOpen: boolean; onClose: () => void; 
           placeholder="Description"
           autoSize={{ minRows: 3, maxRows: 5 }}
       />
+      {alert && (
+        <Alert
+          message="Please enter a title or description."
+          type="error"
+          className="mt-4"
+        />
+      )}
       </>
     </Modal>
   );
